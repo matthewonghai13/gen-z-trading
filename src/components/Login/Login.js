6a767988d38fb2160d925ce6032139b196381581
Matthew <mattonghai@gmail.com>
2021-10-30 14:26:51 -0500

    login without error handling

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,31 +4,23 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import Button from "react-bootstrap/Button";
 import firebase from "firebase/compat/app";
-import "firebase/firestore";
-import "firebase/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
 
 export default function Login({}) {
-  // const [user] = useAuthState(auth);
   const auth = firebase.auth();
+  const [user] = useAuthState(auth);
   const handleLogin = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider);
     console.log("login!");
   };
 
-  function SignOut() {
-    return (
-      auth.currentUser && (
-        <button className="sign-out" onClick={() => auth.signOut()}>
-          Sign Out
-        </button>
-      )
-    );
-  }
-
   return (
-    <div id="loginBody">
-      <Button onClick={handleLogin}>hello</Button>
-    </div>
+    !user && (
+      <div id="loginBody">
+        <Button onClick={handleLogin}>hello</Button>
+      </div>
+    )
   );
 }
